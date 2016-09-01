df00c79b704543f9528b215aaa96edadbeb4fe05
shivamdixit1000 <shivamdixit1000@gmail.com>
2016-09-01 17:14:58 +0530

    Update StudentController.java

--- src/main/java/package1/StudentController.java
+++ src/main/java/package1/StudentController.java
@@ -5,6 +5,8 @@ package package1;
  */
 
 
+// messi is the best
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
